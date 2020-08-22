7c9b1d953214351c54925c305efaf9cd113965c0
hazimhadis89 <hazim.hadis@gmail.com>
2020-08-22 18:44:46 +0800

    - move User rules array to Model

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Model\User;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UserRequest extends FormRequest
@@ -23,24 +24,7 @@ class UserRequest extends FormRequest
      */
     public function rules()
     {
-        $rules = [
-            'name' => '{SOMETIMES}required|string|max:255',
-            'email' => '{SOMETIMES}required|email:rfc,dns|max:255|unique:users{UNIQUE}',
-        ];
-
-        if ($this->isMethod('PUT')) {
-            // replace {SOMETIMES} & {UNIQUE}
-            $rules['name'] = str_replace('{SOMETIMES}', 'sometimes|', $rules['name']);
-            $rules['email'] = str_replace('{SOMETIMES}', 'sometimes|', $rules['email']);
-            $user = $this->route('user');
-            $rules['email'] = str_replace('{UNIQUE}', ",email,$user", $rules['email']);
-        } else {
-            // remove {SOMETIMES} & {UNIQUE}
-            $rules['name'] = str_replace('{SOMETIMES}', '', $rules['name']);
-            $rules['email'] = str_replace('{SOMETIMES}', '', $rules['email']);
-            $rules['email'] = str_replace('{UNIQUE}', '', $rules['email']);
-        }
-
-        return $rules;
+        $id = $this->route('user');
+        return User::rules($this->method(), $id);
     }
 }
--- app/Model/User.php
+++ app/Model/User.php
@@ -37,4 +37,49 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @param $valid // method or action
+     * @param null $id
+     * @return array|null
+     */
+    public static function rules($valid, $id = null)
+    {
+        $valid = strtoupper($valid);
+        if (!in_array($valid, ['PUT', 'POST', 'CREATE', 'UPDATE', 'DELETE'])) {
+            return null;
+        }
+
+        $rules = [
+            'name' => '{SOMETIMES}required|string|max:255',
+            'email' => '{SOMETIMES}required|email:rfc,dns|max:255|unique:users{UNIQUE}',
+        ];
+
+        switch ($valid) {
+            case 'PUT':
+            case 'UPDATE':
+                // replace {SOMETIMES} & {UNIQUE}
+                $rules['name'] = str_replace('{SOMETIMES}', 'sometimes|', $rules['name']);
+                $rules['email'] = str_replace('{SOMETIMES}', 'sometimes|', $rules['email']);
+                $rules['email'] = str_replace('{UNIQUE}', ",email,$id", $rules['email']);
+                break;
+            case 'POST':
+            case 'CREATE':
+                // remove {SOMETIMES} & {UNIQUE}
+                $rules['name'] = str_replace('{SOMETIMES}', '', $rules['name']);
+                $rules['email'] = str_replace('{SOMETIMES}', '', $rules['email']);
+                $rules['email'] = str_replace('{UNIQUE}', '', $rules['email']);
+                break;
+            case 'DELETE':
+                // remove name validation
+                unset($rules['name']);
+                // rewrite email validation
+                $rules['email'] = 'required|email:rfc,dns|max:255';
+                break;
+            default:
+                $rules = null;
+        }
+
+        return $rules;
+    }
 }
